0e2c5fe489cb13d8c5b60b62f83e73f13d74ec90
AiGaf1 <barabaris464@gmail.com>
2021-11-26 21:23:31 +0100

    Revert "Revert "new algo""
    This reverts commit 0899c126ecf5e17e97aa7f01562c349fcff520b5.

---
+++ Algorithms/greedyAlgoVersion2.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def greedyAlgoVersion2(adj_matrix):
+
+    
+    best_length = np.inf
+    best_route = []
+
+    city_lst = [i for i in range(len(adj_matrix))]
+    
+    for cur_city in city_lst: #n
+        route = [cur_city] 
+        length = 0
+        while len(route) != len(adj_matrix):
+            sorted_ind_cities = np.argsort(adj_matrix[cur_city]) # n * log(n)     
+
+            for next_city in sorted_ind_cities: #n  # searching for the smallest distance  
+                if next_city not in route: # 1 * n
+                    route.append(next_city)
+                    length += adj_matrix[cur_city][next_city]
+                    cur_city = next_city
+                    break
+        
+        length += adj_matrix[route[-1]][cur_city]
+        route.append(cur_city)
+
+        if best_length > length:
+            best_length = length
+            best_route =  route
+
+    return best_route, best_length
+
+
+#n^3 *  log(n)
+
+
+
